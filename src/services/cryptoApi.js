db8961d2bee3923976313aba0b7945425399ffb2
Tarun <57524319+tarun19991121@users.noreply.github.com>
2022-07-16 22:08:01 +0530

    Add files via upload

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,28 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+    'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
+    'X-RapidAPI-Key': process.env.REACT_APP_COIN_RANKING_KEY
+}
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders })
+
+export const cryptoApi = createApi({
+    reducerPath: 'cryptoApi',
+    baseQuery: fetchBaseQuery({ baseUrl }),
+    endpoints: (builder) => ({
+        getCryptos: builder.query({
+            query: (count) => createRequest(`/coins?limit=${count}`),
+        }),
+        getCryptoDetails: builder.query({
+            query: (coinId) => createRequest(`/coin/${coinId}`),
+        }),
+        getExchanges: builder.query({
+            query: () => createRequest('/exchanges'),
+        })
+    })
+});
+
+export const { useGetCryptosQuery, useGetCryptoDetailsQuery, useGetExchangesQuery } = cryptoApi;
